90d83617adfc73af6f243ed66add3abdea2f62fc
icer233 <tommydai0909@163.com>
2025-01-30 22:06:17 +0800

    20250130: clean up useles files

--- jiechi.py
+++
@@ -1,9 +0,0 @@
-from mitmproxy import http
-import json
-
-def response(flow: http.HTTPFlow) -> None:
-    if flow.request.pretty_url == "https://www.aiwenyun.cn/yxt/servlet/antiScreenRecord/nct/getScreenRecordList":
-        flow.response.text = json.dumps({
-            "code": 0,
-            "obj": [],
-            "execTime": 8})
